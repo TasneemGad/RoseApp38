881c928a4e45790f02eca2762a1ffc3d96c88cd8
Tasneem-Gad <tgad@sirius-its.com>
2026-08-17 21:01:42 +0200

    fix routing

--- apps/roseApp/src/app/app.routes.ts
+++ apps/roseApp/src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: '',
+    path: 'roseApp',
     loadChildren: () =>
       import('./remote-entry/entry.routes').then((m) => m.remoteRoutes),
+  },
+  {
+    path: '',
+    redirectTo: 'roseApp',
+    pathMatch: 'full',
   },
 ];
